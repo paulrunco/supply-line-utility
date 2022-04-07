453fe3d4c8649be8c7bc5bd975f86fc3928084f0
Paul Runco <runco.paul@gmail.com>
2022-04-07 15:41:37 -0400

    Inititial commit

---
+++ app.py
@@ -0,0 +1,131 @@
+from cProfile import label
+from tkinter import Label, Entry, Button, Menu, StringVar, filedialog, Tk, END
+from tkinter import messagebox as mb
+import webbrowser
+
+from click import command
+
+import build_report
+
+
+class App(Tk):
+    def __init__(self):
+        super().__init__()
+
+        ## App Settings
+        self.resizable(False, False)
+        self.iconbitmap('icon.ico')
+
+        self.version = "0.1"
+        self.author = "PRunco"
+
+        ## Menu
+        self.title("Supply Line Report Utility")
+        self.menubar = Menu(self)
+
+        self.filemenu = Menu(self.menubar, tearoff=False)
+        self.filemenu.add_command(label="Exit", command=self.quit)
+        self.menubar.add_cascade(label="File", menu=self.filemenu)
+        
+        self.helpmenu = Menu(self.menubar, tearoff=False)
+        self.helpmenu.add_command(label="About", command=self.open_about)
+        self.helpmenu.add_command(label="Documentation", command=self.open_docs)
+        self.menubar.add_cascade(label="Help", menu=self.helpmenu)
+
+        self.config(menu=self.menubar)
+
+        ## Order Input
+        # Label
+        self.order_status_label = Label(
+            self,
+            text="Order Status View",
+            ).pack(padx=(5,0), anchor='w')
+        # Entry
+        self.order_status_entry = Entry(
+            self, 
+            width=80, 
+            textvariable=StringVar
+            )
+        self.order_status_entry.pack(padx=5, pady=(0, 5))
+        # Button
+        self.order_status_button = Button(
+            self, text="Browse", 
+            command=lambda: self.browse_for("order_status")
+            ).pack(padx=5, pady=(0, 5), anchor='e')
+
+        ## Template Input
+        # Label
+        self.template_label = Label(
+            self,
+            text="Supply Line Template"
+            ).pack(padx=(5,0), anchor='w')
+        # Entry
+        self.template_entry = Entry(
+            self,
+            width=80,
+            textvariable=StringVar
+            )
+        self.template_entry.pack(padx=5, pady=(0, 5))
+        # Button
+        self.template_button = Button(
+            self, text="Browse", 
+            command=lambda: self.browse_for("template")
+            ).pack(padx=5, pady=(0, 5), anchor='e')
+
+        self.generate_button = Button(
+            self, text="Create report",
+            command=lambda: self.on_click_create_report()
+        ).pack(fill='x', padx=5, pady=5)
+
+    def browse_for(self, target):
+        file_name = filedialog.askopenfilename(
+            filetypes=(("Excel files", "*xlsx"), ("All files", "*"))
+        )
+        if target == "order_status":
+            self.order_status_entry.config(background='white')
+            self.order_status_entry.delete(0, END)
+            self.order_status_entry.insert(0, file_name)
+        if target == 'template':
+            self.template_entry.config(background='white')
+            self.template_entry.delete(0, END)
+            self.template_entry.insert(0, file_name)
+
+    def open_about(self):
+        mb.showinfo(
+            title="About",
+            message=f"Version {self.version} | {self.author}"
+            )
+
+    def open_docs(self):
+        webbrowser.open('https://www.github.com/paulrunco')
+        
+    def ask_save_as(self):
+        save_as = filedialog.asksaveasfilename(
+            title="Save as",
+            filetypes=(("Excel files", "*xlsx"), ("All files", "*")),
+            defaultextension="xlsx",
+            initialfile="test")
+        if save_as:
+            print(save_as)
+            return save_as
+
+    def on_click_create_report(self):
+        path_to_order_status_report = self.order_status_entry.get()
+        if path_to_order_status_report == "":
+            mb.showwarning(title="Warning: ID-10T", message="Please select an Order Status View")
+            self.order_status_entry.config(background= 'red')
+            return
+
+        path_to_template = self.template_entry.get()
+        if path_to_template == "":
+            mb.showwarning(title="Warning: ID-10T", message="Please select the Supply Line Template")
+            self.template_entry.config(background='red')
+            return
+        
+        #save_as = self.ask_save_as()
+        build_report.build_report(self, path_to_order_status_report, path_to_template)
+
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
---
+++ build_report.py
@@ -0,0 +1,93 @@
+import pandas as pd
+from datetime import timedelta
+
+
+def build_report(self, path_to_order_status_report, path_to_supply_line_template):
+
+    sheet_name = "Direct FG Work Orders"
+    columns = ['Customer Part', 'Assembly ID', 'Order-Rel', 'Start Date', 'Est Finish Date', 'Quantity', 'Type']
+
+    old_row_count =  pd.read_excel(path_to_supply_line_template, sheet_name=1, skiprows=16).shape[0]
+
+    # Read Web & customer part numbers (using customer report template for lookup)
+    part_numbers = pd.read_excel(path_to_supply_line_template, sheet_name=0, skiprows=13)
+    part_numbers = part_numbers.drop(
+        columns=[
+            "Quantity_On_Hand",
+            "Lead_Time",
+        ]
+    ).set_index("Supplier_Item_Code")
+
+    # Read order Statuses, drop uneccessary columns, and rename others to customer convention
+    order_status = pd.read_excel(
+        path_to_order_status_report,
+        dtype={"Order No": str, "Rel No": int, "Quantity": int},
+    )
+
+    # Format order dates
+    order_status["Est Finish Date"] = pd.to_datetime(
+        order_status["Est Finish Date"]
+    ).dt.date
+
+    order_status["Start Date"] = order_status["Est Finish Date"] - timedelta(
+        days=7
+    )
+
+    # Format work order as: production order - release
+    order_status["Rel No"] = order_status["Rel No"].map("{:03.0f}".format)
+    order_status["Order-Rel"] = order_status[["Order No", "Rel No"]].agg(
+        "-".join, axis=1
+    )
+
+    # Lookup customer part number using Web part number
+    order_status["Customer Part"] = order_status["Assembly ID"].map(
+        part_numbers["FG_Item_Code"]
+    )
+
+    # Order status / type
+    order_status["Type"] = order_status["Status"].map(
+        {"Firm Planned": "PLANNED", "In Process": "WIP"}
+    )
+
+    # Remove unnececssary columns
+    order_status = order_status.drop(
+        columns=[
+            "Order No",
+            "Rel No",
+            "Customer ID",
+            "Revision Number",
+            "Sales Order",
+            "Customer PO Number",
+            "Location ID",
+            "Planner",
+            "Status",
+            "Est Start Date",
+        ]
+    )
+
+    order_status = order_status[columns] # Reorder columns
+
+    # Append blank rows to end of new data if old data is longer than new data
+    new_row_count = order_status.shape[0]
+
+    if new_row_count < old_row_count:
+        print(f'New data is {old_row_count - new_row_count} smaller than old data, appending blank rows')
+        blank_row = ["" for x in [i for i in range(0,7)]]
+        for i in range(0, old_row_count - new_row_count):
+            order_status.loc[order_status.shape[0]] = blank_row
+
+    ## Save new report into the provided template
+    with pd.ExcelWriter(
+        path_to_supply_line_template,
+        mode="a",
+        if_sheet_exists="overlay",
+        date_format="yyyy-mm-dd",
+        datetime_format="yyyy-mm-dd",
+    ) as writer:
+        order_status.to_excel(
+            writer,
+            sheet_name="Direct FG Work Orders",
+            startrow=17,
+            header=False,
+            index=False,
+        )
